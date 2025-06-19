9921d39368a4c14eb82e053d95121db57345846c
hoshigakikisame <rianatgithub@gmail.com>
2025-06-19 20:28:48 +0700

    feat: update version to 0.0.2.

--- internal/runner/version.go
+++ internal/runner/version.go
@@ -1,14 +1,3 @@
 package runner
 
-import (
-	"os"
-
-	"github.com/projectdiscovery/gologger"
-)
-
-const version = "0.0.1"
-
-func showVersion() {
-	gologger.Print().Msgf("nwp v%s", version)
-	os.Exit(2)
-}
+const version = "0.0.2"
